498159585926e2374c214a06cbf5f6700ec0abb9
Sam Hertz <samhertz@vt.edu>
2022-06-30 09:14:20 -0700

    Initial Commit

---
+++ AsyncAwaitPattern/AsyncAwaitPatternApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncAwaitPatternApp.swift
+//  AsyncAwaitPattern
+//
+//  Created by Samuel Hertz on 6/30/22.
+//
+
+import SwiftUI
+
+@main
+struct AsyncAwaitPatternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
